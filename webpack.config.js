50b4a7b61bd9f2d0dc0ff9a42b4080487e6cd70d
Michael Richardson <michael@richardsons.co.za>
2022-08-31 22:04:15 +0100

    add ts webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -2,73 +2,93 @@ import webpack from 'webpack';
 
 export default [
 	{
-		target: 'web',
-		entry: './index.js',
+		target: "web",
+		entry: "./index.ts",
 		output: {
+			filename: "index.js",
+			path: new URL("./dist/web", import.meta.url).pathname,
+			libraryExport: "default",
+			libraryTarget: "window",
+			library: "Surreal",
 			clean: true,
-			filename: 'index.js',
-			path: new URL('./dist/web', import.meta.url).pathname,
-			libraryExport: 'default',
-			libraryTarget: 'window',
-			library: 'Surreal'
 		},
 		devtool: false,
 		plugins: [
 			new webpack.NormalModuleReplacementPlugin(
 				/..\/websocket\/index.js$/,
-				'/src/websocket/index.web.js',
-			)
+				"/src/websocket/index.web.js"
+			),
 		],
+		resolve: {
+			extensions: [".ts", ".js"],
+		},
 		module: {
-			rules: [{
-				test: /\.js$/,
-				exclude: /(node_modules)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env'],
-						plugins: [
-							'@babel/plugin-proposal-class-properties',
-							'@babel/plugin-proposal-private-methods',
-						],
-					}
-				}
-			}]
-		}
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /(node_modules)/,
+					use: {
+						loader: "babel-loader",
+						options: {
+							presets: ["@babel/preset-env"],
+							plugins: [
+								"@babel/plugin-proposal-class-properties",
+								"@babel/plugin-proposal-private-methods",
+							],
+						},
+					},
+				},
+				{
+					test: /\.tsx?/,
+					use: "ts-loader",
+					exclude: /node_modules/,
+				},
+			],
+		},
 	},
 	{
-		target: 'node',
-		entry: './index.js',
+		target: "node",
+		entry: "./index.ts",
 		output: {
+			filename: "index.cjs",
+			path: new URL("./dist/lib", import.meta.url).pathname,
+			libraryExport: "default",
+			libraryTarget: "umd",
+			library: "Surreal",
 			clean: true,
-			filename: 'index.cjs',
-			path: new URL('./dist/lib', import.meta.url).pathname,
-			libraryExport: 'default',
-			libraryTarget: 'umd',
-			library: 'Surreal'
 		},
 		devtool: false,
 		plugins: [
 			new webpack.NormalModuleReplacementPlugin(
 				/..\/websocket\/index.js$/,
-				'/src/websocket/index.node.js',
-			)
+				"/src/websocket/index.node.js"
+			),
 		],
+		resolve: {
+			extensions: [".ts", ".js"],
+		},
 		module: {
-			rules: [{
-				test: /\.js$/,
-				exclude: /(node_modules)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env'],
-						plugins: [
-							'@babel/plugin-proposal-class-properties',
-							'@babel/plugin-proposal-private-methods',
-						],
-					}
-				}
-			}]
-		}
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /(node_modules)/,
+					use: {
+						loader: "babel-loader",
+						options: {
+							presets: ["@babel/preset-env"],
+							plugins: [
+								"@babel/plugin-proposal-class-properties",
+								"@babel/plugin-proposal-private-methods",
+							],
+						},
+					},
+				},
+				{
+					test: /\.tsx?/,
+					use: "ts-loader",
+					exclude: /node_modules/,
+				},
+			],
+		},
 	},
 ];
